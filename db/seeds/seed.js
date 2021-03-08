b1bb4530bd6677ce2076d04bc8943a23505abb0b
DCrawley94 <duncan.crawley@gmail.com>
2021-03-08 14:20:55 +0000

    Step 1 complete

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,8 +3,32 @@ const {
   articleData,
   commentData,
   userData,
-} = require('../data/index.js');
+} = require( '../data/index.js' );
 
-exports.seed = function (knex) {
+
+exports.seed = function ( knex ) {
+  return knex.migrate.rollback().then( () => {
+    
+  })
+  //
+
+
+
+
+
   // add seeding functionality here
 };
+
+
+/*
+exports.seed = function(knex) {
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      return knex('houses')
+        .insert(houseData)
+        .returning('*');
+    })
+    .then((houseRows) => {
+      // <-- do the rest of the 

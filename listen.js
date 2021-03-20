cdff626c3015e4a948714fffa944df58c0781e82
Scott Oster <artificialingenious@gmail.com>
2021-03-20 00:26:01 +0000

    reformatted PORT process env troubleshooting

--- listen.js
+++ listen.js
@@ -1,4 +1,4 @@
 const app = require("./app");
 console.log(process.env.PORT);
-const { PORT = 9090 } = process.env.PORT;
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, console.log(`listening on port ${PORT}`));

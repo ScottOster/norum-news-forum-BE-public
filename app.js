c11054004d01736b6942194035f9bd134ce75280
Scott Oster <artificialingenious@gmail.com>
2021-04-06 15:04:39 +0100

    added cors middleware to enable front end requests

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const apiRouter = require("./routes/apiRouter.js");
+const express = require('express');
+const apiRouter = require('./routes/apiRouter.js');
+const cors = require('cors');
 
 const {
   psqlThrownErrorHandler,
   customErrorHandler,
   serverErrorHandler,
-} = require("./errorHandlers");
+} = require('./errorHandlers');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use("/api", apiRouter);
+app.use('/api', apiRouter);
 
 app.use(psqlThrownErrorHandler);
 app.use(customErrorHandler);
